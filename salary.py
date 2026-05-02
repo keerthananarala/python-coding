d0000a77c8f0fdde0c141d33c7fbf3ae9346907d
keerthananarala <keerthana6j@gmail.com>
2026-05-02 12:01:55 +0530

    Create salary.py

---
+++ salary.py
@@ -0,0 +1,6 @@
+name=input("enter cmp name:")
+sal=int(input("enter sal:   "))
+design=input("enter position: ")
+d={"TL":10,"manager":20,"fresher":5,"developer":15}
+tax=sal * [d[design]/100]
+print(tax)
